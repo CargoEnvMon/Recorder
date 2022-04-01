950b5f403bc7ee5c31c4a2fffa10d0a1ea451570
bymse <egorstrelov@gmail.com>
2022-04-01 22:26:45 +0300

    Mock data

--- app/cargo_env_mon.cpp
+++ app/cargo_env_mon.cpp
@@ -7,9 +7,31 @@
 #include <freertos/FreeRTOS.h>
 #include "wifilistener.h"
 #include "cargo_env_mon.h"
+#include "measurements_storage.h"
 #include <freertos/task.h>
 
 void cargo_env_mon_setup() {
+    const Measurement first = {
+            .temperature = 5.1,
+            .humidity = 3.3,
+            .timeShift = 20,
+    };
+    save_measurement(first);
+
+    const Measurement second = {
+            .temperature = 8.1,
+            .humidity = 7.3,
+            .timeShift = 21,
+    };
+    save_measurement(second);
+
+    const Measurement third = {
+            .temperature = 15.1,
+            .humidity = 5.3,
+            .timeShift = 19,
+    };
+    save_measurement(third);
+
     xTaskCreatePinnedToCore(
             [](void *) {
                 wifi_loop();
