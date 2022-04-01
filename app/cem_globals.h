7d29678a16219a93b14690786a87f20731a002e0
bymse <egorstrelov@gmail.com>
2022-04-01 22:24:20 +0300

    Recorder client impl

--- app/cem_globals.h
+++ app/cem_globals.h
@@ -5,5 +5,7 @@
 #define WIFI_PASSWORD "123456"
 #define CARGO_ID "stu-324"
 #define MAX_MEASUREMENTS 1000
+#define RECORDER_IP "192.168.1.33"
+#define RECORDER_PORT 9021
 
 #endif //APP_CEM_GLOBALS_H
--- app/recorder_client.cpp
+++ app/recorder_client.cpp
@@ -1,7 +1,35 @@
+#include <WiFiClient.h>
 #include "recorder_client.h"
 #include "cem_globals.h"
 #include "measurements_storage.h"
+#include <Arduino.h>
 
 void send_data() {
+    int length;
+    const auto items = get_measurements(&length);
+
+    if (length <= 0) {
+        return;
+    }
     
+    WiFiClient client;
+    if (!client.connect(RECORDER_IP, RECORDER_PORT)) {
+        return;
+    }
+    String header = CARGO_ID"|1648410051380|3";
+    client.println(header);
+
+    for (int index = 0; index < length; ++index) {
+        const auto item = items[0];
+        String line = "";
+        line.concat(item.timeShift);
+        line.concat("|");
+        line.concat(item.temperature);
+        line.concat("|");
+        line.concat(item.humidity);
+        
+        client.println(line);
+    }
+    
+    client.print("<EOF>");
 }
